636c7f656d7a361f619989f9f1bf08b699ee146b
RamaAW <ramawibi123@gmail.com>
2022-02-16 21:16:46 +0700

    broadcast

---
+++ app/src/main/java/apps/example/mybroadcast/MainActivity.java
@@ -0,0 +1,47 @@
+package apps.example.mybroadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static BroadcastReceiver mCustomReceiver = new CustomReceiver();
+    private Button sendButton;
+    private String ACTION_CUSTOM_BROADCAST = "ACTION_CUSTOM_BROADCAST";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_POWER_CONNECTED);
+        intentFilter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+
+        registerReceiver(mCustomReceiver, intentFilter);
+
+        sendButton = findViewById(R.id.send_btn);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent broadcastIntent = new Intent(ACTION_CUSTOM_BROADCAST);
+                broadcastIntent.putExtra("data", "this is message you should show");
+                LocalBroadcastManager.getInstance(view.getContext()).sendBroadcast(broadcastIntent);
+            }
+        });
+
+        IntentFilter customIntent = new IntentFilter(ACTION_CUSTOM_BROADCAST);
+        LocalBroadcastManager.getInstance(this).registerReceiver(mCustomReceiver, customIntent);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mCustomReceiver);
+    }
+}
